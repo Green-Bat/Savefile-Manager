a3a94cc91122921daf2cef74de448750e6b8b97f
Green-Bat <greenbat777@gmail.com>
2022-02-07 22:33:18 +0200

    Initial branch commit

---
+++ SavefileManager.py
@@ -0,0 +1,16 @@
+# Tkinter modules
+from tkinter import *
+from tkinter import ttk, messagebox, filedialog
+
+# general modules
+from datetime import datetime, time, timedelta
+import os
+from os import path
+
+
+class SavefileManager:
+    def __init__(self, root):
+        # keep a refrence of the base window
+        self._root = root
+
+        pass
